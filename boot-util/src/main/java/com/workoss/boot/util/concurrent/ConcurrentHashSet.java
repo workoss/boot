001365ce290132bffba31b1a1408fc1fc9260037
workoss <lfwork@qq.com>
2020-08-26 16:12:06 +0800

    ConcurrentHashSet add @author

--- boot-util/src/main/java/com/workoss/boot/util/concurrent/ConcurrentHashSet.java
+++ boot-util/src/main/java/com/workoss/boot/util/concurrent/ConcurrentHashSet.java
@@ -28,6 +28,10 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+/**
+ * ConcurrentHashSet
+ * @author workoss
+ */
 public class ConcurrentHashSet<E> extends AbstractSet<E> implements Set<E>, java.io.Serializable {
 
 	private static final long serialVersionUID = -8672117787651310382L;
