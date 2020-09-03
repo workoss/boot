76f5871683d2c5d3889f3a3c6bb6debb3c59f771
workoss <workoss@icloud.com>
2020-09-03 21:54:59 +0800

    remove license title email

--- boot-util/src/main/java/com/workoss/boot/util/reflect/ReflectUtils.java
+++ boot-util/src/main/java/com/workoss/boot/util/reflect/ReflectUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2020-2021 workoss (workoss@icloud.com)
+ * Copyright © 2020-2021 workoss (WORKOSS)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
